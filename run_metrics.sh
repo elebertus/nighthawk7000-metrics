8397617ef3fa151e9ea6c93103f85fdd8892caed
Eric Black <eblack@eblac1ml3.riotgames.com>
2018-03-30 10:08:35 -0700

    janky shell environment calling python, etc

--- run_metrics.sh
+++ run_metrics.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true; do
-  /opt/app/metrics_collector.py
+  METRICS_USER=$METRICS_USER METRICS_PASSWORD=$METRICS_PASSWORD METRICS_URL=$METRICS_URL /opt/app/metrics_collector.py
   sleep 10
 done
